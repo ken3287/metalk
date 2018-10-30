a5739d5d138a673bf8cd0ae885b1b114a2a8a96c
福森賢 <kenkenken0328@icloud.com>
2018-10-30 18:19:21 +0900

    Erb to haml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20181030090340) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
