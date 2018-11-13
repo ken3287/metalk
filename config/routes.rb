bd9112cad2ccb78f2ece812db088f44885e6eb3e
福森賢 <kenkenken0328@icloud.com>
2018-11-13 21:24:28 +0900

    Edit routing
    Nest posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :edit, :update] do
+    resources :posts, only: [:index, :edit, :show]
+  end
 end
