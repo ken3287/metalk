e01ad1f5f3255300e063876bbe95fd62e34a2680
福森賢 <kenkenken0328@icloud.com>
2018-11-14 22:40:31 +0900

    Edit routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :edit, :update] do
-    resources :posts, only: [:index, :new, :edit, :show]
+    resources :posts, only: [:index, :new, :create, :edit, :show]
   end
 end
