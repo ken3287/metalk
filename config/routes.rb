4a58b916d762af44cf7209c82c0e44cad05870d6
福森賢 <kenkenken0328@icloud.com>
2018-11-19 23:08:56 +0900

    Edit root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'post#index'
+  root 'posts#index'
   resources :users, only: [:index, :edit, :update] do
     resources :posts, only: [:index, :new, :create, :edit, :show]
   end
