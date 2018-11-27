38dc501f95c6bdebc47d2dfdf406cec596140785
福森賢 <kenkenken0328@icloud.com>
2018-11-27 18:59:07 +0900

    Edit diary.rb
    Validation

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,6 @@
 class Diary < ApplicationRecord
+  validates :title,    length: { in: 1..14 }
+  validates :sentence,    length: { in: 1..400 }
   default_scope -> { order(created_at: :desc) }
   belongs_to :user
 end
