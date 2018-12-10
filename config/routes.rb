7d29a0f5e2e9341c8103c1e0548e8bf291e3c536
福森賢 <kenkenken0328@icloud.com>
2018-12-10 16:28:50 +0900

    Edit routing
    postでdestroy actionを使うので変更しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :users, only: [:index, :edit, :update] do
-    resources :posts, only: [:index, :new, :create, :edit, :show]
+    resources :posts, only: [:index, :new, :create, :edit, :destroy]
     resources :diaries
   end
 end
