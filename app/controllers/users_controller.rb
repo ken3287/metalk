8c625c4ad9263f8a4c4d2a02b935475f817d3ad1
福森賢 <kenkenken0328@icloud.com>
2018-12-20 14:02:55 +0900

    Delete before_action set_user
    Users controllerのset_controllerは必要ないので削除しました。
    それに伴い
    Usersのeditの@user_infoを削除し、@userに変更しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :confirm_user, only: [:edit, :update]
-  before_action :set_user, only: [:edit, :update]
 
   def index
     @user = User.find_by(id: params[:id])
@@ -11,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    unless @user_info.update(user_params)
+    unless @user.update(user_params)
       redirect_to user_diaries_path(current_user.id)
     else
       redirect_to edit_user_path(current_user.id), notice: '編集できませんでした'
@@ -25,15 +24,11 @@ class UsersController < ApplicationController
   end
 
   def confirm_user
-    @user = current_user.id
-      if current_user.id == @user
+    @user = User.find_by(id: params[:id])
+      if current_user.id == @user.id
       else
         redirect_to new_user_session_path
       end
-  end
-
-  def set_user
-    @user_info = User.find_by(id: params[:id])
   end
 
 end
